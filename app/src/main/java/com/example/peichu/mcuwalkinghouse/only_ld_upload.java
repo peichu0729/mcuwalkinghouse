5c735b07ddadbe6eef78caea618c8b0e68862c01
peichu0729 <nicole862929@gmail.com>
2018-07-14 14:25:25 +0800

    調整註冊畫面0714

--- app/src/main/java/com/example/peichu/mcuwalkinghouse/only_ld_upload.java
+++ app/src/main/java/com/example/peichu/mcuwalkinghouse/only_ld_upload.java
@@ -1,7 +1,9 @@
 package com.example.peichu.mcuwalkinghouse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class only_ld_upload extends Activity {
 
@@ -9,6 +11,10 @@ public class only_ld_upload extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_only_ld_upload);
+    }
+    public void gotopicture(View v){
+        Intent it=new Intent(this,only_ld_picture.class);
+        startActivity(it);
     }
 
 }
